ac30606743245c00746d753799969b46a3c93cc2
RampriyaKarthick <118220230+RampriyaKarthick@users.noreply.github.com>
2023-05-19 00:53:44 +0200

    cookie change

--- config/index.js
+++ config/index.js
@@ -39,8 +39,10 @@ module.exports = (app) => {
     saveUninitialized: false,
     
     cookie: {
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-      secure: process.env.NODE_ENV === "production",
+      //sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      //secure: process.env.NODE_ENV === "production",
+      sameSite:"lax",
+      secure:false,
       httpOnly: true,
       maxAge: 60000000,
 
